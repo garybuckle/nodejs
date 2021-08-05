9842ebf60b79496ffb242d2565b7ca77beba1451
Gary Buckle <buckle.g@mac.com>
2021-08-05 14:17:53 +0100

    Init set  up of Proxies

--- building-website-nodejs-express/routes/index.js
+++ building-website-nodejs-express/routes/index.js
@@ -4,11 +4,12 @@ const feedbackRoute = require('./feedback');
 
 const router = express.Router();
 
-module.exports = params => {
+module.exports = (params) => {
   // Route for default page
   router.get('/', (request, response) => {
-    //   response.sendFile(path.join(__dirname, './static/index.html'));
-    response.render('pages/index', { pageTitle: 'Welcome to Express server learning' });
+    if (!request.session.visitCount) {
+      request.session.visitCount = 0;
+    }
   });
   router.use('/speakers', speakersRoute(params));
   router.use('/feedback', feedbackRoute(params));
--- building-website-nodejs-express/server.js
+++ building-website-nodejs-express/server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 
 const path = require('path');
+const cookieSession = require('cookie-session');
 const FeedbackService = require('./services/FeedbackService');
 const SpeakerService = require('./services/SpeakerService');
 const feedbackService = new FeedbackService('./data/feedback.json');
@@ -14,6 +15,16 @@ const routes = require('./routes/');
 const app = express();
 
 const port = 3000;
+
+// cookies
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['asda123', 'asda1234'],
+  })
+);
 
 // templating engine
 app.set('view engine', 'ejs');
